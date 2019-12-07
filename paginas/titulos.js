884f7fbd462e367e3e8eb7b169603a4f90b55b47
aluno21573 <marya98@sapo.pt>
2019-12-07 16:47:30 +0000

    ddff

---
+++ paginas/titulos.js
@@ -0,0 +1,10 @@
+
+var xpto = document.getElementByID("pt");
+//df=5;
+df="loja de batatas";
+xpto.innerHTML = df;	
+
+var xpto = document.getElementByID("br");
+//df=5;
+df1="Brasilno seu melhor";
+xpto.innerHTML = df1;	
